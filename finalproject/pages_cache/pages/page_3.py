d5559c03653b32fec6d7a735f643cc087c86385d
rabdullahi <65207620+rabdullahi@users.noreply.github.com>
2022-12-11 18:33:41 -0500

    Update page_3.py

--- finalproject/pages_cache/pages/page_3.py
+++ finalproject/pages_cache/pages/page_3.py
@@ -1,1 +1,35 @@
+import streamlit as st
+import pandas as pd 
+from PIL import Image
+import numpy as np
+import plotly_express as px
 
+
+
+
+
+
+mall = pd.read_csv("/Users/saldana/Documents/streamlit_env/finalproject/Mall_Customers.csv")
+
+st.title('Annual Income')
+st.header('Line Chart by Age and Annual Income')
+
+
+
+
+
+
+
+m=px.bar(mall, x="Age", y="Annual Income (k$)", color="Annual Income (k$)")
+st.plotly_chart(m)
+
+st.caption('In general, people of the age of 32, have a higher annual income. Also, we can see that the 30 age group on average has a higher annual income. Individuals who are 60+ has a lower annual income in comparison to other ages. ')
+
+
+
+st.header('Box Plot by Gender and Annual Income')
+
+z=px.box(mall, x="Gender", y="Annual Income (k$)", points="all", color="Gender")
+st.plotly_chart(z)
+
+st.caption('The average annual income for male is slightly higher compared to females. Based on the range, the annual income is more dispersed for males.')
